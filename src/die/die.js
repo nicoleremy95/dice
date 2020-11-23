b762d7ed20fc11609b9c08a946c3a7ac655d9770
Nicole Remy <nicoleremy95@gmail.com>
2020-11-23 14:51:16 -0800

    die and rolldice components set up

---
+++ src/die/die.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Die extends Component{
+    render(){
+        return(
+            <div className="Die">
+
+            </div>
+        )
+    }
+}
+
+export default Die;
--- src/roll_dice/RollDice.js
+++ src/roll_dice/RollDice.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class RollDice extends Component{
     render(){
         return(
+            <div className="RollDice">
 
+            </div>
         )
     }
 }
